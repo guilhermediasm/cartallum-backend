1e26b5a8ffc0aaaecdd94514fe763ecb3e68d52f
Guilherme Dias <guilherme.dias@codificar.com.br>
2020-04-05 17:44:13 -0300

    Add rota index

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+require('./controllers/web')(app)
 
 require('./controllers/authController')(app)
 require('./controllers/registerController')(app)
